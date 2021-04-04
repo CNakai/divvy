99bba79aed6a2ff8f935886a9598bef06d3f4d2f
Colter Nakai McAddis <cnm62@nau.edu>
2021-04-03 22:14:03 -0700

    Refactor the SubmissionFileFactory away to some class/static methods
    I'm not sure whether this is the right move or not, but it seems easily
    reversible if necessary.  This increases the depth of the SubmissionFile class
    and coherently groups the BBLearn filename validation code into the class that
    is most likely to use it... unless I separated the validation out and ran it
    all at once up front... hmm...

--- tests/test_bblearn_submissionfile.py
+++ tests/test_bblearn_submissionfile.py
@@ -1,26 +1,62 @@
 # TODO: do interface test for these a la Sandi Metz?
-from divvy.bblearn import SubmissionFileFactory as SFF
+from divvy.bblearn import SubmissionFile as SF
 from pytest import fixture
+from pytest import raises
 
-class SubmissionFileDouble:
-    submitted_for = 'Homework 3_ Math quiz modifications'
-    submitted_on = '2021-03-17-12-08-23'
-    submitted_by = 'smp549'
-    submitted_as = 'Math quiz documentation.pdf'
-    bblearn_name = (
-        f"{submitted_for}_{submitted_by}_attempt_{submitted_on}_{submitted_as}"
-    )
+@fixture
+def valid_bblearn_filename():
+    return ("Homework 3_ Math quiz modifications_smp549_attempt_" +
+            "2021-03-17-12-08-23_Math quiz documentation.pdf")
+
+
+@fixture
+def bblearn_submission_manifest_filename():
+    return ("Homework 3_ Math quiz modifications_smp549_attempt_" +
+            "2021-03-17-12-08-23.txt")
+
+
+@fixture
+def invalid_bblearn_filename(bblearn_submission_manifest_filename):
+    return bblearn_submission_manifest_filename
 
 
 @fixture
 def test_double():
+    class SubmissionFileDouble:
+        submitted_for = 'Homework 3_ Math quiz modifications'
+        submitted_on = '2021-03-17-12-08-23'
+        submitted_by = 'smp549'
+        submitted_as = 'Math quiz documentation.pdf'
+        bblearn_name = (
+            f"{submitted_for}_{submitted_by}_attempt_{submitted_on}_{submitted_as}"
+        )
+
     return SubmissionFileDouble()
 
 
 @fixture
 def test_submission_file(test_double):
-    return SFF.from_bblearn_filename(test_double.bblearn_name)
+    return SF(submitted_as=test_double.submitted_as,
+              submitted_by=test_double.submitted_by,
+              submitted_on=test_double.submitted_on,
+              submitted_for=test_double.submitted_for)
 
+
+def test_validates_valid_bblearn_filename(valid_bblearn_filename):
+    assert SF.is_valid_bblearn_filename(valid_bblearn_filename)
+
+
+def test_invalidates_valid_bblearn_filename(invalid_bblearn_filename):
+    assert not SF.is_valid_bblearn_filename(invalid_bblearn_filename)
+
+
+def test_creation_from_valid_bblearn_filename_succeeds(valid_bblearn_filename):
+    SF.from_bblearn_filename(valid_bblearn_filename)
+
+
+def test_creation_from_invalid_bblearn_filename_fails(invalid_bblearn_filename):
+    with raises(Exception):
+        SF.from_bblearn_filename(invalid_bblearn_filename)
 
 def test_retrieves_bblearn_name(test_submission_file, test_double):
     assert test_submission_file.bblearn_name == test_double.bblearn_name
