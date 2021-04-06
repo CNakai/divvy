01e9f5a29550bde0b65e0786d20e15ecb4c023f1
Colter Nakai McAddis <cnm62@nau.edu>
2021-04-06 14:40:44 -0700

    Remove last bblear_submission_utils dependency anticipating refactor

--- divvy/rubrics/collate.py
+++ divvy/rubrics/collate.py
@@ -1,12 +1,12 @@
-from ..bblearn_submission_utils import refile_files_by_submitter
 import click
 from json import dump, load
 from os import mkdir, remove, rename
 from os.path import abspath, basename, exists, join, split
 import pathlib
-from shutil import copy, copytree, make_archive, rmtree
+from shutil import copy, copytree, make_archive, move, rmtree
 from sys import exit
 from zipfile import is_zipfile, ZipFile
+
 
 
 # TODO
@@ -166,3 +166,59 @@ def package_rubrics(output_dir):
             root_dir=output_dir,
             base_dir=gradee_rubrics_path.name
         )
+
+
+def refile_files_by_submitter(submission_directory, alias_map=None,
+                              unpack_submitted_zips=False):
+    """
+    Creates per student subdirectories within the submission_directory and
+    moves each student's files into their directory.
+
+    If there is no alias map the per student subdirectories will be named
+    according to their BBLearn username, otherwise their username will be
+    looked up in the alias map and the alias will be used instead.
+
+    If unpack_submitted_zips is true, after the submitted files have been moved
+    into the created per student subdirectories, those subdirectories will be
+    visited and any zip files found in them will be extracted into them and
+    then the zip file will be deleted.
+    """
+    submission_file_paths = sorted(
+        list(submission_directory.iterdir()),
+        key=lambda path: extract_student_id(path.name)
+    )
+
+    for path in submission_file_paths:
+        username = extract_bblearn_username(path.name)
+        alias = alias_map[username] if alias_map else username
+        student_directory = submission_directory.joinpath(alias)
+        if not student_directory.exists():
+            mkdir(student_directory)
+
+        file_name_as_submitted = remove_bblearn_prefix(path.name)
+        move(path, student_directory.joinpath(file_name_as_submitted))
+
+    for student_directory_path in submission_directory.iterdir():
+        # Clean up metadata files
+        for cruft in ['__MACOSX', '.DS_store']:
+            rmtree(student_directory_path.joinpath(cruft), ignore_errors=True)
+
+        if unpack_submitted_zips:
+            for submitted_file_path in student_directory_path.iterdir():
+                if is_zipfile(submitted_file_path):
+                    with ZipFile(submitted_file_path) as z:
+                        z.extractall(student_directory_path)
+
+
+def extract_bblearn_username(bblearn_submitted_filename):
+    return extract_student_id(bblearn_submitted_filename)
+
+
+def extract_student_id(bblearn_filename):
+    front = bblearn_filename.split(sep='_attempt_', maxsplit=2)[0]
+    return front[front.rindex('_') + 1:]
+
+
+def remove_bblearn_prefix(bblearn_filename):
+    back = bblearn_filename.split(sep='_attempt_', maxsplit=2)[1]
+    return back[back.find('_') + 1:]
